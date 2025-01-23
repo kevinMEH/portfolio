9350732b63c8ac5aefe32ee9e9b27da0f896d5dc
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2025-01-23 09:45:47 -0500

    Tailwind and styling setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,29 @@
 import type { Metadata } from "next";
+import { Inter, Fira_Code } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
     title: "Portfolio | Kevin Liao",
 };
 
+const inter = Inter({
+    subsets: ["latin"],
+    display: "block",
+    variable: "--font-inter"
+});
+
+const mono = Fira_Code({
+    subsets: ["latin"],
+    display: "block",
+    variable: "--font-mono"
+});
+
 export default function RootLayout({
     children,
 }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <html lang="en">
-            <body>
+        <html lang="en" className={`${inter.variable} ${mono.variable}`}>
+            <body className="bg-bg-dark text-main font-inter">
                 {children}
             </body>
         </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,27 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        colors: {
+            "transparent": "transparent",
+            "current": "currentColor",
+            
+            "bg-dark": "#0F1314",
+            "bg-mid": "#171B1C",
+            
+            "main": "#EFF8FA",
+            "sub": "#808889",
+            "highlight": "#3F8071"
+        },
+        fontFamily: {
+            "inter": ["var(--font-inter)"],
+            "mono": ["var(--font-mono)"]
+        }
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
