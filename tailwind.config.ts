aafc905a85b7e6e6989729a39e59848cc1f6f2b5
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2025-01-24 11:20:33 -0500

    Tailwind scan data.json

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ export default {
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./custom/data.json"
     ],
     theme: {
         colors: {
