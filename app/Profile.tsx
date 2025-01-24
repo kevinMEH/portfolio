05adfb1c5276b1de8bd0a575fefab20332b0ab34
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2025-01-24 11:17:38 -0500

    Profile.tsx

---
+++ app/Profile.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import ProfilePicture from "@/custom/profile.png";
+
+export default function Profile() {
+    return <div className="w-full flex justify-around -mb-8">
+        <div className="w-[min(40%,128px)] min-w-[100px]">
+            <Image
+                src={ProfilePicture}
+                alt="Profile Picture"
+                className="rounded-full border-bg-dark border-[6px] -ml-[6px] -mt-[max(18%,80px)]"
+            />
+        </div>
+    </div>
+}
