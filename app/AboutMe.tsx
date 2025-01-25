46af1a41422d23c6b13f8c9170282de0250ed828
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2025-01-25 05:43:15 -0500

    Unified Profile.tsx into AboutMe.tsx

--- app/AboutMe.tsx
+++ app/AboutMe.tsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
+import Image from "next/image";
+
 import { getDescription, getTag, getTitle, getUrl } from "./data";
+
+import ProfilePicture from "@/custom/profile.png";
 
 export default async function AboutMe() {
     const [ tag, url, title, description ] = await Promise.all([
@@ -10,6 +14,13 @@ export default async function AboutMe() {
     ]);
 
     return <div className="flex flex-col items-center gap-2 text-center pb-2">
+        <div className="w-[min(37%,140px)] min-w-[100px] pb-2">
+            <Image
+                src={ProfilePicture}
+                alt="Profile Picture"
+                className="rounded-full outline outline-bg-dark outline-[6px] sm:outline-8 -mt-[80px]"
+            />
+        </div>
         <Link href={url} target="_blank" className="text-sm sm:text-base font-mono text-sub leading-none">
             { tag }
         </Link>
