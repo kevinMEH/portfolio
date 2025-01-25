476fa5a5214574996eb412f7919047107956f1a7
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2025-01-25 03:08:17 -0500

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import AboutMe from "./AboutMe";
+import Banner from "./Banner";
+import Contact from "./Contact";
+import Profile from "./Profile";
+import Projects from "./Projects";
+import Technologies from "./Technologies";
+
 export default function Portfolio() {
-    return <div></div>;
+    return (
+        <div>
+            <Banner />
+            <div className="px-6 space-y-12 pt-4 pb-8">
+                <Profile />
+                <AboutMe />
+                <Technologies />
+                <Projects />
+                <Contact />
+            </div>
+        </div>
+    );
 }
