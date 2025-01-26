5f9eab5778fed794b4012adcf852295a3caa02ff
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2025-01-26 05:34:18 -0500

    Dark mode script modification

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata, Viewport } from "next";
 import { Inter, Fira_Code } from "next/font/google";
 import { getWebsiteTitle } from "./data";
 import "./globals.css";
-import Script from "next/script";
 
 export const metadata: Metadata = {
     title: await getWebsiteTitle(),
@@ -34,31 +33,31 @@ export default function RootLayout({
     children,
 }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <html lang="en" className={`${inter.variable} ${mono.variable}`}>
-            <Script id="theme-script" strategy="beforeInteractive">
-                {`
-                (function() {
-                    function setTheme(theme) {
-                        if(theme === "dark") document.documentElement.classList.add("dark");
-                        else document.documentElement.classList.remove("dark");
-                    }
-                    
-                    function systemQueryHandler(event) {
-                        if(localStorage.getItem("theme") === null)
-                            setTheme(event.matches ? "dark" : "light");
-                    }
-                    
-                    let preferredTheme = localStorage.getItem("theme");
-                    if(preferredTheme === null) {
-                        let systemDarkQuery = window.matchMedia("(prefers-color-scheme: dark)");
-                        systemQueryHandler(systemDarkQuery);
-                        systemDarkQuery.addEventListener("change", systemQueryHandler);
-                    } else {
-                        setTheme(preferredTheme);
-                    }
-                })(); 
-                `}
-            </Script>
+        <html lang="en" className={`${inter.variable} ${mono.variable}`} suppressHydrationWarning={true}>
+            <head>
+                <script dangerouslySetInnerHTML={{ __html:
+                    `(function() {
+                        function setTheme(theme) {
+                            if(theme === "dark") document.documentElement.classList.add("dark");
+                            else document.documentElement.classList.remove("dark");
+                        }
+                        
+                        function systemQueryHandler(event) {
+                            if(localStorage.getItem("theme") === null)
+                                setTheme(event.matches ? "dark" : "light");
+                        }
+                        
+                        let preferredTheme = localStorage.getItem("theme");
+                        if(preferredTheme === null) {
+                            let systemDarkQuery = window.matchMedia("(prefers-color-scheme: dark)");
+                            systemQueryHandler(systemDarkQuery);
+                            systemDarkQuery.addEventListener("change", systemQueryHandler);
+                        } else {
+                            setTheme(preferredTheme);
+                        }
+                    })();`
+                }} />
+            </head>
             <body className="bg-bg-dark text-main font-inter">
                 {children}
             </body>
