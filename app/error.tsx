7d83cfa86bc6df03df8d09283f8eb5065d31ad43
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2025-01-26 10:37:03 -0500

    Custom error handling

---
+++ app/error.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Error() {
+    return <div className="flex items-center justify-center font-mono h-full">An unknown error has occurred.</div>;
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,3 @@
+export default function NotFound() {
+    return <div className="flex items-center justify-center font-mono h-full">404: Page not found.</div>;
+}
