a09198aa051e6d40ec3535f3690cb12925250049
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2025-08-22 03:23:43 -0400

    Image qualities configuration

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
+const imageQualities = [];
+for(let i = 50; i <= 100; i+= 5) {
+    imageQualities.push(i);
+}
+
 const nextConfig: NextConfig = {
     images: {
-        imageSizes: [32, 48, 64, 96, 128, 140, 192, 256, 280, 384, 420]
+        imageSizes: [32, 48, 64, 96, 128, 140, 192, 256, 280, 384, 420],
+        qualities: imageQualities,
     }
 };
 
